5ddf8e7f1a3eb60c1d93729771d8afd8630faf49
MatMark <mateusz@hexadronic.com>
2021-10-23 19:07:16 +0200

    1 zadanie

---
+++ lab1/lab1.py
@@ -0,0 +1,60 @@
+import numpy as np
+from numpy.lib.function_base import append
+from tabulate import tabulate
+
+
+def user_input():
+    while(True):
+        val = input("Podaj p dla ciała GF(p): ")
+        try:
+            p = int(val)
+            if(p <= 19):
+                break
+            else:
+                print("p musi być liczbą całkowitą ≤ 19")
+        except ValueError:
+            print("p musi być liczbą całkowitą ≤ 19")
+    return p
+
+
+def add_table(p):
+    # appearance only
+    table = np.zeros(shape=[p, p + 1])
+    headers = ['+']
+    for i in range(0, p + 1):
+        headers = append(headers, i)
+        if(i < p):
+            table[i, 0] = i
+
+    # main functionality
+    for i in range(0, p + 1):
+        if(i < p):
+            for j in range(0, p + 1):
+                if(j < p):
+                    table[j, i+1] = (i + j) % p
+
+    print(tabulate(table, headers, tablefmt="fancy_grid"))
+
+
+def mul_table(p):
+    # appearance only
+    table = np.zeros(shape=[p, p + 1])
+    headers = ['*']
+    for i in range(0, p + 1):
+        headers = append(headers, i)
+        if(i < p):
+            table[i, 0] = i
+
+    # main functionality
+    for i in range(0, p + 1):
+        if(i < p):
+            for j in range(0, p + 1):
+                if(j < p):
+                    table[j, i+1] = (i * j) % p
+
+    print(tabulate(table, headers, tablefmt="fancy_grid"))
+
+
+p = user_input()
+add_table(p)
+mul_table(p)
