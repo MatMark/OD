e5c6adceed412ffbaf2be0eceaa3be8e0e5c40c8
MatMark <mateusz@hexadronic.com>
2021-11-14 22:27:07 +0100

    2 zadanie

---
+++ lab2/helpers.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def headings(size: int):
+    x = np.array(np.ones((1, size), dtype=int), dtype=str)
+    y = np.array(np.ones((1, size + 1), dtype=int), dtype=str)
+    for i in range(2, size):
+        x[0][i] = 'a'+str(i-1)
+        y[0][i+1] = 'a'+str(i-1)
+    x[0][0] = 0
+    y[0][0] = '*'
+    y[0][1] = 0
+    return x, y
+
+
+def menu(options: dict):
+    print('\n'.join('{}: {}'.format(k, v.__name__)
+                    for k, v in options.items()))
+    val = user_input_max('Wybierz zadanie: ', max=len(options)+1)
+    if(val == 0):
+        return
+    else:
+        options[val]()
+
+
+def user_input_max(text: str, max: int = None):
+    while(True):
+        val = input(text)
+        try:
+            val = int(val)
+            if(val <= max):
+                break
+            else:
+                print("Wartosc musi być liczbą całkowitą ≤ {}".format(max))
+        except ValueError:
+            print("Wartosc musi być liczbą całkowitą ≤ {}".format(max))
+    return val
+
+
+def user_input(text: str):
+    while(True):
+        val = input(text)
+        try:
+            val = int(val)
+            break
+        except ValueError:
+            print("Wartosc musi być liczbą całkowitą")
+    return val
--- lab2/lab2.py
+++ lab2/lab2.py
@@ -2,18 +2,23 @@ import math
 
 import numpy as np
 from tabulate import tabulate
+from helpers import headings, user_input, menu
 
 
-def headings(size: int):
-    x = np.array(np.ones((1, size), dtype=int), dtype=str)
-    y = np.array(np.ones((1, size + 1), dtype=int), dtype=str)
-    for i in range(2, size):
-        x[0][i] = 'a'+str(i-1)
-        y[0][i+1] = 'a'+str(i-1)
-    x[0][0] = 0
-    y[0][0] = '*'
-    y[0][1] = 0
-    return x, y
+ZechTable = {
+    4: (2, 1),
+    8: (3, 6, 1, 5, 4, 2),
+    16: (4, 8, 14, 1, 10, 13, 9, 2, 7, 5, 12, 11, 6, 3)
+}
+
+
+def inverse(x: int, p: int, q: int):
+    if (x != 0 and p > 2):
+        return int((x + (q - 1) / 2) % (q - 1))
+    elif (x != 0 and p == 2):
+        return x
+    elif (x == 0):
+        return 0
 
 
 def mulTable(p: int, m: int):
@@ -34,6 +39,39 @@ def mulTable(p: int, m: int):
     return table
 
 
-# 1 zadanie
-print(tabulate((mulTable(2, 3)), tablefmt="fancy_grid"))
-print(tabulate((mulTable(2, 4)), tablefmt="fancy_grid"))
+def add(x: int, y: int, p: int, m: int):
+    q = int(math.pow(p, m))
+    if (y > x):
+        x, y = y, x
+    if (x != 0 and y != 0 and x > y):
+        zech = ZechTable.get(q)[x - y - 1]
+        return ((y + zech - 1) % (q - 1) + 1)
+    elif (y == 0 or x == 0):
+        return x + y
+    elif (x != 0 and y == inverse(x, p, q)):
+        return 0
+
+
+def mul(x: int, y: int, p: int, m: int):
+    q = int(math.pow(p, m))
+    if (x > 0 and y > 0):
+        return 1 + (x + y - 2) % (q - 1)
+    else:
+        return 0
+
+
+def zad1():
+    print(tabulate((mulTable(2, 3)), tablefmt="fancy_grid"))
+    print(tabulate((mulTable(2, 4)), tablefmt="fancy_grid"))
+
+
+def zad2():
+    x = user_input('Pierwsza liczba: ')
+    y = user_input('Druga liczba: ')
+    p = user_input('p: ')
+    m = user_input('m: ')
+    print('{} + {} ='.format(x, y), add(x, y, p, m))
+    print('{} * {} ='.format(x, y), mul(x, y, p, m))
+
+
+menu({1: zad1, 2: zad2})
